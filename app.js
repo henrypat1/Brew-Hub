ea5b9aebc8c4d65f8714f8fa80f44a27eb3c2a5a
Henry Patterson <henrycpatterson92@gmail.com>
2021-06-22 23:29:52 -0400

    go penguins

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const m
+const Domain = "https://api.openbrewerydb.org/breweries?by_state=";
+
+const searQuries = [];
+
+let text = document.querySelector ('#search')
+let input = document.querySelector('#blank')
+text.addEventListener('click')
