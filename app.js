18aba0b503bdf35847b9411fbd07db541477e81d
Henry Patterson <henrycpatterson92@gmail.com>
2021-06-23 20:31:14 -0400

    go penguins

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-const DOMAIN = "https://api.openbrewerydb.org/breweries?by_state=";
+// const DOMAIN = "https://api.openbrewerydb.org/breweries?by_state=";
 
-const searQuries = [];
 
-let text = document.querySelector ('#search')
-let input = document.querySelector('#blank')
-text.addEventListener('click', storeText)
+const form = document.querySelector('form')
+const breweryData = document.querySelector('#brewery-data')
 
-let article = document.querySelector('aritcle')
-
-function storeText() {
-  getState(input)
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const input = document.querySelector('#state-input').value
   console.log(input)
-}
+  getState(input)
+})
 
-async function getState(input) {
+
+async function getState(state) {
   try {
-    const response = await axios.get(`${DOMAIN}s=${input.value}`);
-    let breweryList = response.data.Search;
-    console.log(breweryList);
-    let i = 0;
-    while (breweryList.length > i) {
-      console.log(breweryList)
-      let breweryDiv = document.createElement('div')
-      ariticle.append(breweryDiv)
-      breweryDiv.classList.add('brewery')
-      breweryDiv.textContent = breweryList(i).Name
-    }
+    const DOMAIN = `https://api.openbrewerydb.org/breweries?by_state=${state}`;
+    const response = await axios.get(DOMAIN);
+    
+    response.data.forEach((brewery) => {
+      console.log(brewery)
+      const breweryName = document.createElement('h1')
+      const breweryStreet = document.createElement('h3')
+      const breweryPhone = document.createElement('h3')
+      const brewerySite = document.createElement('h3')
+      breweryName.textContent = brewery.name
+      breweryStreet.textContent = brewery.street
+      breweryPhone.textContent = brewery.phone
+      brewerySite.textContent = brewery.website_url
+      breweryData.append(breweryName)
+      breweryData.append(breweryStreet)
+      breweryData.append(breweryPhone)
+      breweryData.append(brewerySite)
+    })
 
   } catch (error) {
     console.error(error)
